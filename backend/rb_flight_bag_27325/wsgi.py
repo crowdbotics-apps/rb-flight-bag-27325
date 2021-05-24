2477f72f285f1eb0de0dfd4a800c4b4b60524e09
Crowdbotics <team@crowdbotics.com>
2021-05-24 14:55:16 +0000

    Update models

--- backend/rb_flight_bag_27325/wsgi.py
+++ backend/rb_flight_bag_27325/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rb_flight_bag_27325.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rb_flight_bag_27325.settings")
 
 application = get_wsgi_application()
