dc73dcb5c93a252cb3063fda8eae546022d50365
Crowdbotics <team@crowdbotics.com>
2021-05-24 14:51:56 +0000

    Initial commit.

---
+++ backend/rb_flight_bag_27325/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for rb_flight_bag_27325 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rb_flight_bag_27325.settings')
+
+application = get_wsgi_application()
